91f27f9e868cc35aa8b77eee68856c13f079960d
rabbithay <thay.csb@gmail.com>
2021-11-20 01:19:56 -0300

    chore: update dependencies and create connections with the databases

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,16 +1,15 @@
 module.exports = {
   env: {
-    browser: true,
     es2021: true,
+    node: true,
   },
   extends: [
     'airbnb-base',
   ],
   parserOptions: {
-    ecmaVersion: 12,
+    ecmaVersion: 13,
     sourceType: 'module',
   },
   rules: {
-    'no-console': 'off',
   },
 };
--- src/server.js
+++ src/server.js
@@ -1,7 +1,5 @@
 import app from './app';
 
-const port = 4002;
-
 process.on('unhandledRejection', (reason, promise) => {
   console.error(`Server exiting due to an unhandled promise rejection: ${promise} and reason ${reason}`);
   throw reason;
@@ -10,6 +8,8 @@ process.on('unhandledRejection', (reason, promise) => {
 process.on('uncaughtException', (error) => {
   console.error('Server exiting due to uncaught exception', error);
 });
+
+const port = process.env.NODE_ENV === 'dev' ? 4000 : process.env.PORT;
 
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}`);
